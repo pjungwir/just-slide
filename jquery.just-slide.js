ebd2a7c657de4c1402451843718dc0e889983724
Paul A. Jungwirth <pj@illuminatedcomputing.com>
2012-08-24 09:48:58 -0700

    Avoid some annoying flashes of background on webkit browsers

--- jquery.just-slide.js
+++ jquery.just-slide.js
@@ -9,12 +9,9 @@
     var $all = $(this);
     $all.each(function() {
       var $t = $(this);
-      $t.data('justslide:current-img', 1);
+      $t.data('justslide:current-img', 0);
       $t.data('justslide:total-imgs', $t.find('img').addClass('justslide-stock').size());
       $t.css('overflow', 'hidden');
-      $t.append($('<img src="' + $t.find('img.justslide-stock').eq(0).attr('src') + '"' +
-        ' style="position:absolute; top:0px; left:0px; z-index:100; display:block"' +
-        ' class="justslide-slide">'));
     });
     return $all;
   };
@@ -25,18 +22,22 @@
       var $t = $(this);
       var i = $t.data('justslide:current-img');
       var len = $t.data('justslide:total-imgs');
+      var i_next = (i + 1) % len;
       var w = $t.width();
       var h = $t.height();
       var $prev = $(this).find('.justslide-slide');
-      var $next = $('<img src="' + $t.find('img.justslide-stock').eq(i).attr('src') + '"' +
-        ' style="position:absolute; top:0px; left:' + w + 'px; z-index:200; display:block"' +
-        ' class="justslide-slide">');
+      var $next = $('<div class="justslide-slide" style="position:absolute; top:0px;' +
+        ' left:0px; z-index:200; width:' + (2*w) + 'px">' +
+        '<img src="' + $t.find('img.justslide-stock').eq(i).attr('src') + '"' + 
+        ' style="margin:0; padding:0; display:inline">' +
+        '<img src="' + $t.find('img.justslide-stock').eq(i_next).attr('src') + '"' + 
+        ' style="margin:0; padding:0; display:inline">' +
+        '</div>');
       $t.append($next);
-      $prev.animate({left: (-w) + 'px'}, 'fast', function() {});
-      $next.animate({left: '0px'}, 'fast', '', function() {
+      $next.animate({left: (-w) + 'px'}, 'fast', '', function() {
         $prev.remove();
         $next.css('z-index', 100);
-        $t.data('justslide:current-img', (i + 1) % len);
+        $t.data('justslide:current-img', i_next);
       });
     });
     return $all;
